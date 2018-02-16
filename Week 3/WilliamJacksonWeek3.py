5fa37d1edfb07697e41480bc24126e7803690caf
William Jackson <wsjackson5@gmail.com>
2018-02-16 14:37:21 -0800

    Tried simultaneous assignment Week 3.

--- Week 3/WilliamJacksonWeek3.py
+++ Week 3/WilliamJacksonWeek3.py
@@ -1,4 +1,7 @@
+import time
+
 def fibonacci(n, a=0, b=1):	#Returns nth number, index starting at 1, of fibonacci sequence starting with a,b.
+	startTime = time.time()
 	i = 1
 	c = b			#Handles case n = 1 or n = 2.
 	if n == 1:
@@ -8,7 +11,20 @@ def fibonacci(n, a=0, b=1):	#Returns nth number, index starting at 1, of fibonac
 		a = b
 		b = c
 		i += 1
+	print("- %s seconds -" % (time.time() - startTime))
 	return c
 
-print(fibonacci(1000, 1, 2))
-		
+def fibonacciB(n, a=0, b=1):	#Returns nth number, index starting at 1, of fibonacci sequence starting with a,b.
+	startTime = time.time()
+	i = 1		#Handles case n = 1 or n = 2.
+	if n == 1:
+		return a
+	while i <= (n-2):
+		a, b = b, a+b
+		i += 1
+	print("- %s seconds -" % (time.time() - startTime))
+	return b
+
+fibonacci(1000000, 1, 2)
+#fibonacciB(2000000, 1, 2)
+	
